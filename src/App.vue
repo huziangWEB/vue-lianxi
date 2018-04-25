40bbade4a49d2601430e6eb2fbdf16f6ed43ea73
huziangWEB <704913915@qq.com>
2018-04-25 22:31:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <footer>
+      <div>
+        <p>
+          <span></span>
+        </p>
+        <p>首页</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
